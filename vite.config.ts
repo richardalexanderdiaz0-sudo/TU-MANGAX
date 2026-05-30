6c6bf74290ffa566d83b793ef4a191dab90ec1c8
richardalexanderdiaz0-sudo <richardalexanderdiaz0@gmail.com>
2026-05-30 10:09:27 -0400

    build: set base path to relative for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig(({mode}) => {
         }
       })
     ],
+    base: './',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
